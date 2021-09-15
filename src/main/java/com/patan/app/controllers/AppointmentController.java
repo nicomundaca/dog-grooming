fde595ade77b84175d52436dbe61762122a9d7fe
Nicolas Mundaca <mundacanicolas@gmail.com>
2021-09-15 09:57:31 -0300

    correcion a los cambios solicitados en las respuestas http

--- src/main/java/com/patan/app/controllers/AppointmentController.java
+++ src/main/java/com/patan/app/controllers/AppointmentController.java
@@ -49,7 +49,7 @@ public class AppointmentController {
     @PostMapping("users/{userID}/appoinments")
     public ResponseEntity<String> addAppointment(@PathVariable("userID") Long userID, @RequestBody AppointmentDTO appointmentDTO) throws CommonException {
         appointmentService.save(userID, appointmentDTO);
-        return new ResponseEntity<>("add appointment OK", HttpStatus.OK);
+        return new ResponseEntity<>("add appointment OK", HttpStatus.CREATED);
     }
 
     @ExceptionHandler(value = CommonException.class)
